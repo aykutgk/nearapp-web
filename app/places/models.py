6682143c8c9df48cd865310e1608aec8d3917ff2
Aykut Gedik <aykutged@gmail.com>
2017-03-31 10:07:49 -0400

    renamed lng to lon

--- app/places/models.py
+++ app/places/models.py
@@ -32,8 +32,8 @@ class Channel(models.Model):
 class Place(models.Model):
     owner = models.ForeignKey(Owner, verbose_name="Business or place owner to manege deals")
     name = models.CharField("Name of the business or place", max_length=255)
+    lon = models.FloatField("Longitude")
     lat = models.FloatField("Latitude")
-    lng = models.FloatField("Longitude")
     google_place_id = models.CharField("Google place id comes from places detail api", max_length=60, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
