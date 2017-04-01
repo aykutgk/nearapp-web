d59e474f223ca286f16d8094b973be1b11bc3bb1
Aykut Gedik <aykutged@gmail.com>
2017-04-01 18:45:24 -0400

    added active field, and google maps static image for lon/lat

--- app/places/admin.py
+++ app/places/admin.py
@@ -1,6 +1,50 @@
+from django.conf import settings
 from django.contrib import admin
+from django.utils.html import format_html
+from django.utils.safestring import mark_safe
 
 from .models import Place
 
+class PlaceAdmin(admin.ModelAdmin):
+    fieldsets = (
+        ('Info', {
+            'classes': ('extrapretty',),
+            'fields': ('owner', 'name')
+        }),
+        ('Location', {
+            'fields': ('lon_lat', 'google_place_id')
+        }),
+        ('Management', {
+            'fields': ('active',)
+        }),
+    )
 
-admin.site.register(Place)
+    def lon_lat(self, obj):
+        lon = obj.point.x
+        lat = obj.point.y
+        google_statics_url = "https://maps.googleapis.com/maps/api/staticmap?"\
+        "center={lon},{lat}"\
+        "&zoom=12&size=800x300&maptype=roadmap"\
+        "&markers=color:red%7Clabel:P%7C{lon},{lat}"\
+        "&key={api_key}".format(
+            lon=lon,
+            lat=lat,
+            api_key=settings.GOOGLE_MAP_API_KEY
+        )
+        return format_html(
+                    (   '<span>Longitude: {}, Latitude: {}</span><br>'
+                        '<a href="https://www.google.com/maps?q=loc:{},{}&zoom=8" '
+                        'target="_blank"><img src="{}" alt="Open google maps"><br>Click to open google maps</a>'
+                    ),
+                    lon,
+                    lat,
+                    lon,
+                    lat,
+                    google_statics_url,
+               )
+    lon_lat.short_description = "Lon/Lat on Google Map"
+
+    readonly_fields = ("lon_lat","google_place_id")
+
+
+admin.site.register(Place, PlaceAdmin)
--- app/places/models.py
+++ app/places/models.py
@@ -31,12 +31,13 @@ class Channel(models.Model):
 
 
 class Place(models.Model):
-    owner = models.ForeignKey(Owner, verbose_name="Business or place owner to manege deals")
+    owner = models.ForeignKey(Owner, verbose_name="Owner")
     name = models.CharField("Name of the business or place", max_length=255)
     point = models.PointField("Longitude/Latitude on Map", default=GEOSGeometry('POINT(0 0)'))
-    google_place_id = models.CharField("Google place id comes from places detail api", max_length=60, unique=True)
+    google_place_id = models.CharField("Google place id", max_length=60, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    active = models.BooleanField("Is this business or place still active?", default=True)
 
     def __str__(self):
         return self.name
